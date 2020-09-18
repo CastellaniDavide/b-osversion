e81e9b95e2fd3e6d5cf696d9b71215c69b3e6070
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-18 09:14:53 +0200

    Created bin/template.py

---
+++ bin/osversion.py
@@ -0,0 +1,19 @@
+"""osversion
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2020-9-18"
+
+class osversion:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(osversion.osversion())
+	
+	def osversion():
+		"""osversion first funtion
+		"""
+		return "osversion" #for the test (see test_osversion.py)
+		
+if __name__ == "__main__":
+	osversion()
