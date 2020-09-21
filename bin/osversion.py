29d6363b3faad49090dc0d9515846308370e8dbf
DavideC03 <davide.castellani.5.8.03@gmail.com>
2020-09-21 16:36:20 +0200

    v01.01

--- bin/osversion.py
+++ bin/osversion.py
@@ -1,6 +1,6 @@
 """osversion
 """
-import os, wmi
+import os, wmi, sqlite3
 from datetime import datetime
 
 __author__ = "help@castellanidavide.it"
@@ -14,19 +14,27 @@ class osversion:
 
 		# Open files
 		log = open(os.path.join(base_dir, "log", "trace.log"), "a")
-		csv_in = open(os.path.join(base_dir, "flussi", "PC_name.csv"), "r")
-		csv_out = open(os.path.join(base_dir, "flussi", "PC_version.csv"), "w+")
+		csv_in = open(os.path.join(base_dir, "flussi", "computers.csv"), "r")
+		csv_out = open(os.path.join(base_dir, "flussi", "osversion.csv"), "w+")
+		db_osversion = sqlite3.connect(os.path.join(base_dir, "flussi", "osversion.db")).cursor()
 
-		osversion.log(log, "Opened all files")
-		osversion.init_csv(csv_in, csv_out, log)
+		osversion.log(log, "Opened all files and database connected")
 		
 		# Start time
 		start_time = datetime.now()
 		osversion.log(log, f"Start time: {start_time}")
 		osversion.log(log, "Running: osversion.py")
 
+		# Init files and db
+		intestation = "PC_name, OS, OS_version"
+		osversion.init_csv(csv_in, csv_out, intestation, log)
+		osversion.init_db(db_osversion, intestation, log)
+
 		# Core operations
-		osversion.print_OS_Info(log, csv_in, csv_out, debug)
+		osversion.print_OS_Info(log, csv_in, csv_out, db_osversion, debug)
+
+		# Check db
+		osversion.check_db(db_osversion, intestation, log)
 
 		# End
 		osversion.log(log, f"End time: {datetime.now()}\nTotal time: {datetime.now() - start_time}")
@@ -35,7 +43,7 @@ class osversion:
 		csv_in.close()
 		csv_out.close()
 
-	def print_OS_Info(log, csv_in, csv_out, debug):
+	def print_OS_Info(log, csv_in, csv_out, db_osversion, debug):
 		"""Prints the infos by Win32_OperatingSystem
 		"""
 		osversion.print_and_log(log, " - Get OS and OS version in:")
@@ -43,12 +51,14 @@ class osversion:
 		# Only on the PC if debug is true, else on all PCs in the list
 		for PC_name in ("My PC, debug option",) if debug else csv_in.read().split("\n")[1:]:
 			# Establish a new connection
-			conn = wmi.WMI("" if debug else PC_name)
+			conn = wmi.WMI("." if debug else PC_name)
 
 			# Get the necessary infos
 			for os_info in conn.Win32_OperatingSystem(["Caption", "Version"]):
 				osversion.print_and_log(log, f"   - {PC_name}")
-				csv_out.write(f"{'My PC' if debug else PC_name}, {os_info.Caption}, {os_info.Version}\n")
+				data = f"'{'My PC' if debug else PC_name}','{os_info.Caption}','{os_info.Version}'"
+				csv_out.write(data.replace("'", ""))
+				db_osversion.execute(f"INSERT INTO osversion VALUES ({data})")
 
 	def log(file, item):
 		"""Writes a line in the log.log file
@@ -61,12 +71,31 @@ class osversion:
 		print(item)
 		osversion.log(file, item)
 
-	def init_csv(init, end, log):
+	def init_csv(init, end, intestation, log):
 		"""Init the csv files
 		"""
-		end.write("PC_name, OS, OS_version\n")
+		end.write(intestation)
 		osversion.log(log, "csv now initialized")
 
+	def init_db(db_osversion, intestation, log):
+		"""Init the database
+		"""
+		try:
+			db_osversion.execute(f'''CREATE TABLE osversion ({intestation})''')
+		except:
+			db_osversion.execute("DROP TABLE osversion")
+			db_osversion.execute(f'''CREATE TABLE osversion ({intestation})''')
+
+		osversion.log(log, "database now initialized")
+
+	def check_db(db_file, intestation, log, tablename="osversion"):
+		"""Checks the database content
+		"""
+		print("\nDatabase content:")
+		print(f"{intestation}")
+
+		for row in db_file.execute(f"SELECT * FROM {tablename}"):
+			print(str(row)[1:-1].replace("'", ""))
 
 if __name__ == "__main__":
 	# debug flag
--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,8 @@
 [![GitHub license](https://img.shields.io/badge/licence-GNU-green?style=flat)](https://github.com/CastellaniDavide/cpp-osversion/blob/master/LICENSE) ![Author](https://img.shields.io/badge/author-Castellani%20Davide-green?style=flat) ![Version](https://img.shields.io/badge/version-v01.01-blue?style=flat) ![Language Python](https://img.shields.io/badge/language-Python-yellowgreen?style=flat) ![sys.platform supported](https://img.shields.io/badge/OS%20platform%20supported-Linux,%20Windows%20&%20Mac%20OS-blue?style=flat) [![On GitHub](https://img.shields.io/badge/on%20GitHub-True-green?style=flat&logo=github)](https://github.com/CastellaniDavide/osversion)
 
 # Description
-Get the os vesioning by a list of PC
+Get the os vesioning by a list of PC.
+The output will be printed in "osversion.csv" file & "osversion.db" database
 
 ## Required
  - python3
@@ -13,16 +14,15 @@ Get the os vesioning by a list of PC
    - ISSUE_TEMPLATE
      - bug_report.md
      - feature-request.md
- - workflows
-   - python-test.yml
  - bin
    - **osversion.py**
  - docs
    - LICENSE
    - README.md
  - flussi
-   - PC_name.csv
-   - PC_version.csv
+   - computers.csv
+   - osversion.csv
+   - osversion.db
  - log
    - trace.log
  - requirements
